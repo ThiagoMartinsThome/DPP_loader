5abf66e6b7ab88c35ae9a3df186283f0afed680f
Thiago.Martins <tthome3@hotmail.com>
2021-06-02 18:26:03 +0200

    commit4  - setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"thiago.martins@europcar.com\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
