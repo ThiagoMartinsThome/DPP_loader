3b8b618209f0c72be35c0c542cc71b0545482a8d
Thiago.Martins <tthome3@hotmail.com>
2021-06-02 19:56:08 +0200

    Commit6 - DPP_Pricing_Platform.py

--- DPP_Pricing_Platform.py
+++ DPP_Pricing_Platform.py
@@ -32,15 +32,16 @@ sheet_name = st.selectbox('Sheet Name', ['dpp_data1',
                                          ])
 #For heroku
 import os
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.binary_location = GOOGLE_CHROME_PATH
 
 if st.button('Hit me'):
     # For heroku
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     # chormedriver_path = 'chromedriver_win32/chromedriver.exe'
     # driver = webdriver.Chrome(executable_path=chormedriver_path)
     sleep(2)
